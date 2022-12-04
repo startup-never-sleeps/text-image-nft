9cfec1587ef30f798a002af846037cb762766c3d
Hlib Pylypets <pilipets.gleb@gmail.com>
2022-12-04 16:15:55 +0000

    OOP refactor to use TextImageDiffusion class + added grid generation

--- stable_diff.py
+++ stable_diff.py
@@ -1,29 +1,57 @@
 import torch
 from diffusers import StableDiffusionPipeline
+from PIL import Image
 
-def get_generator_pipe(model_id='CompVis/stable-diffusion-v1-4'):
-	if torch.cuda.is_available():
-		device = 'cuda'
-		kwargs = {'revision':'fp16', 'torch_dtype':'torch.float16'}
-	else:
-		device = 'cpu'
-		kwargs = {}
 
-	pipe = StableDiffusionPipeline.from_pretrained(
-		model_id,
-		use_auth_token=True,
-		**kwargs
-	)
+class TextImageDiffusion:
+	def __init__(self, model_id='CompVis/stable-diffusion-v1-4'):
+		self.pipe = TextImageDiffusion.get_diffusion_pipe(model_id)
 
-	return pipe.to(device)
+	def genImage(self, description, num_images=1, num_rows=1):
+		if num_images == 1:
+			return self.pipe(description).images[0]
+		else:
+			description = [description] * num_images
+			images = self.pipe(description).images
+
+			num_cols = num_images // num_rows
+			grid = TextImageDiffusion.get_image_grids(images, rows=num_rows, cols=num_cols)
+			return grid
+
+	@staticmethod
+	def get_image_grids(images, rows, cols):
+		assert len(images) == rows*cols
+
+		width, height = images[0].size
+		grid = Image.new('RGB', size=(cols*width, rows*height))
+		
+		for i, img in enumerate(img):
+			grid.paste(img, box=(i%cols*width, i//cols*height))
+		return grid
+
+	@staticmethod
+	def get_diffusion_pipe(model_id):
+		if torch.cuda.is_available():
+			device = 'cuda'
+			kwargs = {'revision':'fp16', 'torch_dtype':'torch.float16'}
+		else:
+			device = 'cpu'
+			kwargs = {}
+
+		pipe = StableDiffusionPipeline.from_pretrained(
+			model_id,
+			use_auth_token=True,
+			**kwargs
+		)
+
+		return pipe.to(device)
 
 
 if __name__ == '__main__':
-	pipe = get_generator_pipe()
+	imageGen = TextImageDiffusion()
 
 	while True:
 		description = input("Enter image desc: ")
 		if description == '__exit__': break
 
-		image = pipe(description).images[0]
-		image.show()
+		image = imageGen.genImage(description, num_images=6, num_rows=2)
